bf184092c6644b1888155577f661ddc839e9e585
xiuda3411 <1157867526@qq.com>
2019-10-02 04:14:00 +0800

    :sparkles:更新

---
+++ src/main/java/cn/edu/cqupt/mislab/work/constant/ResultEnum.java
@@ -0,0 +1,44 @@
+package cn.edu.cqupt.mislab.work.constant;
+
+/**
+ * @program: work-Mis.Lab
+ * @description: 返回值
+ * @author: 宋丽
+ * @create: 2019-09-13 16:20
+ **/
+public enum  ResultEnum {
+    /**
+     * 状态码记录
+     * 0----成功
+     * 1----失败
+     * 2----不存在
+     * 3----已存在
+     * 4----参数为空
+     * 5----用户未登陆
+     */
+    SUCCESS(0,"成功"),
+    ERROR(1,"失败"),
+    NOTEXIST(2,"不存在"),
+    ISEXIST(3,"已存在"),
+    ISNULL(4,"参数为空"),
+    NOLOGIN(5,"用户未登录"),
+    NODISCUSS(6,"没有评论"),
+    NOREPLY(7,"没有回复"),
+    ;
+
+    private Integer status;
+    private String msg;
+
+    ResultEnum(int status,String msg){
+        this.status=status;
+        this.msg=msg;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
