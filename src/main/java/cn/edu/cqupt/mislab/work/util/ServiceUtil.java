03cb8d48a01e9cc957be38e9d854c8a1b9bb3c82
xiuda3411 <1157867526@qq.com>
2019-10-02 04:30:27 +0800

    :zap:服务层工具类

---
+++ src/main/java/cn/edu/cqupt/mislab/work/util/ServiceUtil.java
@@ -0,0 +1,45 @@
+package cn.edu.cqupt.mislab.work.util;
+
+import cn.edu.cqupt.mislab.work.constant.ResultEnum;
+import cn.edu.cqupt.mislab.work.exception.MyException;
+
+/**
+ * @program: work-Mis.Lab
+ * @description: 业务逻辑工具
+ * @author: 宋丽
+ * @create: 2019-09-18 08:30
+ **/
+public class ServiceUtil {
+    /**
+     * update业务逻辑工具
+     * @param flag
+     * @throws MyException
+     */
+    public static void updateSuccess(boolean flag) throws MyException {
+        if(!flag){
+            throw new MyException(ResultEnum.ERROR);
+        }
+    }
+
+    /**
+     * delete业务逻辑工具
+     * @param flag
+     * @throws MyException
+     */
+    public static void deleteSuccess(boolean flag) throws MyException {
+        if(!flag){
+            throw new MyException(ResultEnum.NOTEXIST);
+        }
+    }
+
+    /**
+     * insert业务逻辑工具
+     * @param flag
+     * @throws MyException
+     */
+    public static void insertSuccess(boolean flag) throws MyException {
+        if(!flag){
+            throw new MyException(ResultEnum.ISEXIST);
+        }
+    }
+}
