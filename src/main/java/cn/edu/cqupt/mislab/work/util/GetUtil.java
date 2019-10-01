521291ff817c1d7274b51cccff1522c1fb7e5c82
xiuda3411 <1157867526@qq.com>
2019-10-02 04:29:35 +0800

    :zap:取值工具类

---
+++ src/main/java/cn/edu/cqupt/mislab/work/util/GetUtil.java
@@ -0,0 +1,19 @@
+package cn.edu.cqupt.mislab.work.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @program: work-Mis.Lab
+ * @description: 获取数据的工具类
+ * @author: 宋丽
+ * @create: 2019-09-19 08:46
+ **/
+public class GetUtil {
+    /**
+     * 从session里获取projectId
+     * @return
+     */
+    public static Integer getUserId(HttpServletRequest request){
+        return (Integer) request.getSession().getAttribute("userId");
+    }
+}
