7f7e6e0fb892ddc27773c798c723404fa80aa4ae
xiuda3411 <1157867526@qq.com>
2019-10-07 08:11:23 +0800

    :memo:完善

--- src/main/java/cn/edu/cqupt/mislab/work/controller/FileController.java
+++ src/main/java/cn/edu/cqupt/mislab/work/controller/FileController.java
@@ -1,12 +1,12 @@
 package cn.edu.cqupt.mislab.work.controller;
 import cn.edu.cqupt.mislab.work.domain.po.Result;
+import cn.edu.cqupt.mislab.work.exception.MyException;
 import cn.edu.cqupt.mislab.work.service.FileService;
+import cn.edu.cqupt.mislab.work.util.ControllerUtil;
+import cn.edu.cqupt.mislab.work.util.GetUtil;
 import cn.edu.cqupt.mislab.work.util.ResultUtil;
-import cn.edu.cqupt.mislab.work.util.UploadUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -30,19 +30,40 @@ public class FileController {
 
     @ApiOperation(value = "文件上传")
     @RequestMapping(value = "/upload",method = RequestMethod.POST)
-    public Result upload(@RequestParam("file") MultipartFile file,@RequestParam("userId")Integer userId, HttpServletRequest request, HttpSession session) {
-            return fileService.uploadFile(file,userId);
+    public Result upload(@RequestParam("file") MultipartFile file, HttpServletRequest request, HttpSession session) {
+        Integer userId = GetUtil.getUserId(request);
+        try {
+            ControllerUtil.userIdIsNull(userId);
+        } catch (MyException e) {
+            e.printStackTrace();
+            return ResultUtil.notLogin();
+        }
+        return fileService.uploadFile(file,userId);
     }
 
     @ApiOperation(value = "文件批量上传")
     @RequestMapping(value = "/batch",method = RequestMethod.POST)
     public Result uploadMultipleFiles(HttpServletRequest request) {
+        Integer userId = GetUtil.getUserId(request);
+        try {
+            ControllerUtil.userIdIsNull(userId);
+        } catch (MyException e) {
+            e.printStackTrace();
+            return ResultUtil.notLogin();
+        }
         return fileService.uploadMultipleFiles(request);
     }
 
     @ApiOperation(value = "文件下载")
     @RequestMapping(value = "/download",method = RequestMethod.GET)
-        public Result downloadFile(Integer fid,HttpServletResponse response) {
+        public Result downloadFile(Integer fid,HttpServletRequest request,HttpServletResponse response) {
+        Integer userId = GetUtil.getUserId(request);
+        try {
+            ControllerUtil.userIdIsNull(userId);
+        } catch (MyException e) {
+            e.printStackTrace();
+            return ResultUtil.notLogin();
+        }
         return fileService.downloadFile(fid,response);
     }
 }
