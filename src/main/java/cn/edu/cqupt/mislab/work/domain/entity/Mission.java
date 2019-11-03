172e57d0639869cfcf655f399538e7888d4b85a5
xiuda3411 <1157867526@qq.com>
2019-11-03 20:18:21 +0800

    Merge remote-tracking branch 'origin/dev-spx'

--- src/main/java/cn/edu/cqupt/mislab/work/domain/entity/Mission.java
+++ src/main/java/cn/edu/cqupt/mislab/work/domain/entity/Mission.java
@@ -59,14 +59,6 @@ public class Mission {
         this.createTime = createTime;
     }
 
-    public String getFileName() {
-        return fileName;
-    }
-
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
-    }
-
     public String getContext() {
         return context;
     }
