5cf7220b45d91b8c860c23aa072a1c3e6b160a71
xiuda3411 <1157867526@qq.com>
2019-11-22 19:51:11 +0800

    返回状态码

---
+++ src/main/java/cn/edu/cqupt/mislab/work/constant/ResultStatu.java
@@ -0,0 +1,10 @@
+package cn.edu.cqupt.mislab.work.constant;
+
+public interface ResultStatu {
+    int SUCCESS = 1;
+    int PARMETER_INVALIATE = 2;
+    int THREE_SERVICE_FILE = 3;
+    int SERVER_EXCUTE_FAIL = 4;
+    int USER_OPERATION_ERROR = 5;
+    int UPLOAD_FAIL = 6;
+}
