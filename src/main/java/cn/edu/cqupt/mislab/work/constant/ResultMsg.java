3a805fda7e50c391c033a8a27ef427d1d84ade83
xiuda3411 <1157867526@qq.com>
2019-11-22 19:50:57 +0800

    返回信息

---
+++ src/main/java/cn/edu/cqupt/mislab/work/constant/ResultMsg.java
@@ -0,0 +1,8 @@
+package cn.edu.cqupt.mislab.work.constant;
+
+public interface ResultMsg {
+    String SUCCESSFUL = "ok";
+    String FAILURE = "error";
+    String FAIL = "FAIL";
+    String SUCCESS = "SUCCESS";
+}
