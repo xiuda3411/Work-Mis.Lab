4cdb16b6c285f9f7d1ce5ae570751a8bcb6622f4
xiuda3411 <1157867526@qq.com>
2019-11-22 19:58:00 +0800

    改错

--- src/main/java/cn/edu/cqupt/mislab/work/dao/FileDao.java
+++ src/main/java/cn/edu/cqupt/mislab/work/dao/FileDao.java
@@ -3,6 +3,11 @@ import cn.edu.cqupt.mislab.work.domain.entity.MyFile;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @program: work-Mis.Lab
  * @description: 文件上传dao层
@@ -14,30 +19,37 @@ import org.springframework.stereotype.Repository;
 public interface FileDao {
     /**
      * 上传文件的各种信息
-     * @param fileName
-     * @param md5
-     * @param filePath
-     * @return
+     * @param fileName 文件名
+     * @param md5 文件的md5值
+     * @param filePath 文件储存的绝对路径
+     * @return true = 文件上传成功 false = 文件上传失败
      */
     Boolean uploadFile(@Param("fileName")String fileName,@Param("filePath")String filePath,@Param("md5")String md5);
     /**
      * 比对md5值是否已存在
-     * @param md5
-     * @return
+     * @param md5 文件的md5值
+     * @return 如果文件已存在返回其md5值，如果文件不存在则返回null
      */
     String comparedMd5(@Param("md5")String md5);
 
     /**
      * 通过文件id获取文件的信息
-     * @param id
-     * @return
+     * @param id 文件id
+     * @return 返回文件的所有信息
      */
     MyFile getFile(@Param("id")Integer id);
     /**
      * 通过文件md5值获取文件的信息
-     * @param md5
-     * @return
+     * @param md5 文件的md5值
+     * @return 返回文件的所有信息
      */
     MyFile getFileByMd5(@Param("md5")String md5);
+
+    /**
+     * 通过文件id获取文件名
+     * @param fileId 文件id
+     * @return 返回文件名
+     */
+    String fileName(@Param("fileId")Integer fileId);
 
 }
