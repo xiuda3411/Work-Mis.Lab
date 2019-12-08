2c86c16b775a1f1f1630418041000cea437aec0b
xiuda3411 <1157867526@qq.com>
2019-12-08 19:19:29 +0800

    更新服务工具类

--- src/main/java/cn/edu/cqupt/mislab/work/util/ServiceUtil.java
+++ src/main/java/cn/edu/cqupt/mislab/work/util/ServiceUtil.java
@@ -2,6 +2,7 @@ package cn.edu.cqupt.mislab.work.util;
 
 import cn.edu.cqupt.mislab.work.constant.ResultEnum;
 import cn.edu.cqupt.mislab.work.exception.MyException;
+import cn.edu.cqupt.mislab.work.exception.ServerException;
 
 /**
  * @program: work-Mis.Lab
@@ -13,33 +14,33 @@ public class ServiceUtil {
     /**
      * update业务逻辑工具
      * @param flag
-     * @throws MyException
+     * @throws ServerException
      */
-    public static void updateSuccess(Boolean flag) throws MyException {
+    public static void updateSuccess(Boolean flag) {
         if(!flag){
-            throw new MyException(ResultEnum.ERROR);
+            throw new ServerException("数据库事务错误，查无此条记录");
         }
     }
 
     /**
      * delete业务逻辑工具
      * @param flag
-     * @throws MyException
+     * @throws ServerException
      */
-    public static void deleteSuccess(Boolean flag) throws MyException {
+    public static void deleteSuccess(Boolean flag){
         if(!flag){
-            throw new MyException(ResultEnum.NOTEXIST);
+            throw new ServerException("数据库事务错误，查无此条记录");
         }
     }
 
     /**
      * insert业务逻辑工具
      * @param flag
-     * @throws MyException
+     * @throws ServerException
      */
-    public static void insertSuccess(Boolean flag) throws MyException {
+    public static void insertSuccess(Boolean flag)  {
         if(!flag){
-            throw new MyException(ResultEnum.ISEXIST);
+            throw new ServerException("数据库事务错误");
         }
     }
 }
